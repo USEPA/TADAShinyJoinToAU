f2493b6df6c1b5cb027794200bc857aad9821ef4
Mullin <Mullin.Cristina@epa.gov>
2025-08-12 14:57:19 -0400

    remove deployment

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -16,14 +16,14 @@
 ## Run checks ----
 ## Check the package before sending to prod
 devtools::check()
-rhub::check_for_cran()
+# rhub::check_for_cran()
 
 # Deploy
 
 ## Local, CRAN or Package Manager ----
 ## This will build a tar.gz that can be installed locally,
 ## sent to CRAN, or to a package manager
-devtools::build()
+# devtools::build()
 
 ## Docker ----
 ## If you want to deploy via a generic Dockerfile
@@ -50,7 +50,7 @@ rsconnect::deployApp(
     # Add any additional files unique to your app here.
     "R/",
     "inst/",
-    "data/",
+    # "data/",
     "NAMESPACE",
     "DESCRIPTION",
     "app.R"
@@ -59,3 +59,23 @@ rsconnect::deployApp(
   lint = FALSE,
   forceUpdate = TRUE
 )
+
+# This is how to deploy to EPA's Posit Connect
+# golem::add_positconnect_file() # already exists
+rsconnect::deployApp(appDir = getwd(),
+                     account = "Cristina", 
+                     appFiles = c("app.R", 
+                                  "DESCRIPTION", 
+                                  "NAMESPACE", 
+                                  "R/", 
+                                  "inst/"),
+                     quarto = FALSE,
+                     server = "rstudio-connect.dmap-stage.aws.epa.gov",      
+                     appName = "tadashinyjointoau", 
+                     appTitle = "TADAShinyJoinToAU",
+                     appId = 979, 
+                     launch.browser = TRUE, 
+                     lint = TRUE, 
+                     metadata = list(asMultiple = FALSE, asStatic = FALSE),      
+                     logLevel = "verbose", 
+                     forceUpdate = TRUE)
