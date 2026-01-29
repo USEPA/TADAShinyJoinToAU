01c5400f7eb0ce3c2337bbf425e5a9e2358bcfd1
Mullin <Mullin.Cristina@epa.gov>
2026-01-29 09:36:32 -0500

    Update app.R
    required change for posit deployment

--- app.R
+++ app.R
@@ -1,9 +1,13 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
+# Launch the ShinyApp (Do not remove this file)
+# To deploy, run the script in 04_deploy.R
 
-pkgload::load_all(export_all = FALSE,
-                  helpers = FALSE,
-                  attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-TADAShinyJoinToAU::run_app() # add parameters here (if any)
+# Suppress Shiny support autoloading (extra safety; not strictly necessary if inst/app has no R/)
+options(shiny.autoload.r = FALSE)
+
+pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+
+# Golem production mode
+options(golem.app.prod = TRUE)
+
+# Start the golem app
+run_app()
